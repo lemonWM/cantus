d4827dd34679c0d64cea61f38d8c790d33ab4fe6
lemonWM <arturchochla@gmail.com>
2020-07-26 15:36:28 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -7,20 +6,18 @@ import store from './store/store'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
 
-
 Vue.config.productionTip = false
 
-// const axiosCustom = axios.create({ś
-//   baseURL: 'https://traveo-vue.herokuapp.com'
-// })
-
 const axiosCustom = axios.create({
-  baseURL: 'http://localhost:5000/'
+  baseURL: 'https://cantus-app.herokuapp.com/'
 })
+
+// const axiosCustom = axios.create({
+//   baseURL: 'http://localhost:5000/'
+// })
 
 Vue.use(VueAxios, axiosCustom)
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
